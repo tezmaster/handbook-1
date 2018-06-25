c8c56fc06e802f8a0c7e35acceb20bf5afe16a57
Luke Morton <lukemorton.dev@gmail.com>
2018-06-25 14:52:44 +0100

    Add Customer Support Rep role to Handbook

---
+++ roles/as_code/lib/shared_expectations/live_services_expectations.rb
@@ -0,0 +1,10 @@
+class LiveServicesExpectations < JobSpec::Role::Expectations
+  expected 'to provide an outstanding customer service',
+    'We expect our Live Services team to be a delightful people to talk to for our customers. Our conversations should be professional and friendly, we should small talk where possible, use a smiley face once in a while and generally be nice folk.'
+
+  expected 'to be a responsible remote worker',
+    'We expect our Live Services team to communicate more than anyone else in the business if they choose to work fully remote. Remote working requires extra attention to be paid to communication channels like email and slack.'
+
+  expected 'to effectively juggle commitments',
+    'We expect our Live Services team to be able to balance the priority of their work. Having an understanding of the severity of work along with expected time to fix is important and it should be used to help prioritise what to work on. Customers expectations need to be set, met and if they cannot be met, be reset early and often.'
+end
